d64977adf18d23cbb2cc4f02e54579e2f77e5acb
jongddak <hfsmsj316@gmail.com>
2024-07-18 12:44:07 +0900

    test
    test

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -24,16 +24,16 @@ namespace GitTest
     struct Item 
     {
         public string Name;
-        public int level;
+        public int Equipmentlevel;
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Player player = new Player() { Name = "용사", level = 1; };   
-            Monster monster = new Monster() { Name="슬라임", hp = 1 };
-            
+            Player player = new Player() { Name = "용사", level = 1 };
+            Monster monster = new Monster() { Name = "슬라임", hp = 1 };
+            Item item = new Item() { Name = "검", Equipmentlevel = 1 };
         }
     }
 }
